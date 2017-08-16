ae0d70e46b84de8308ac1a3583c3de06ee8df4e3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-15 22:02:18 -0500

    Number of Airplanes in the Sky

--- number-of-airplanes-in-the-sky.cpp
+++ number-of-airplanes-in-the-sky.cpp
@@ -34,6 +34,43 @@ public:
 	 */
 	int countOfAirplanes(vector<Interval> &airplanes) {
 		// write your code here
+		sort(begin(airplanes), end(airplanes), Comp());
+		priority_queue<int, vector<int>, greater<int>> pq;
+		int result = 0, sz = airplanes.size();
+		for (int i = 0; i < sz; ++i) {
+			while (!pq.empty() and pq.top() <= airplanes[i].start) {
+				pq.pop();
+			}
+			pq.push(airplanes[i].end);
+			result = max(result, (int)pq.size());
+		}
+		return result;
+	}
+private:
+	struct Comp {
+		bool operator() (const Interval & a, const Interval & b) {
+			return a.start < b.start;
+		}
+	};
+};
+
+/**
+ * Definition of Interval:
+ * classs Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ */
+class Solution {
+public:
+	/**
+	 * @param intervals: An interval array
+	 * @return: Count of airplanes are in the sky.
+	 */
+	int countOfAirplanes(vector<Interval> &airplanes) {
+		// write your code here
 		vector<array<int, 2>> A;
 		for (const auto & i : airplanes) {
 			A.push_back({i.start, 1});
